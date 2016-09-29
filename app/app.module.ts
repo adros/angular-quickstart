f4743a7e785e6745b436e436a0e1fcbae86f6aea
Adrian Rakovsky <adrian.rakovsky@gratex.com>
2016-09-29 09:59:15 +0200

    init

---
+++ app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import './rxjs-extensions';
+
+import { routing } from './app.routing';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }   from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroSearchComponent }     from './hero-search.component';
+
+
+import { HeroService } from './hero.service';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        routing
+    ],
+    declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent, HeroSearchComponent],
+    bootstrap: [AppComponent],
+    providers: [HeroService]
+})
+export class AppModule { }
